bab577ef772b625cd227e8df78f536fc11edb430
amberjane10 <pelayomaryjane13@gmail.com>
2024-11-28 19:17:10 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// JavaScript to toggle the navigation menu on smaller screens
+document.getElementById('hamburger').addEventListener('click', function() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('active');
+  });
+  
